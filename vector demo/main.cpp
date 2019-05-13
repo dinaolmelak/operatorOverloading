231b1c30589f91919d1ea200bae83926f69441ef
Dinaol Tadesse <dinaolmelak@gmail.com>
2019-05-12 21:45:47 -0400

    Got the Operator Overloading to work

--- vector demo/main.cpp
+++ vector demo/main.cpp
@@ -23,18 +23,23 @@ public:
         return salary;
     }
     person operator+(person aPerson){
-        
         person sample;
-        sample.salary = salary+aPerson.salary;
+        sample.fullName = this->fullName +" and "+ aPerson.fullName;
+        sample.age = this->age + aPerson.age;
+        sample.salary= this->salary + aPerson.salary;
+        //sample.salary = salary+aPerson.salary;
+        //sample.fullName = sample.fullName + " "+ aPerson.fullName;
+        //sample.age = sample.age + aPerson.age;
+        
         return sample;
     }
     
-    friend ostream& operator<<(person &printable, ostream& out);
+    friend ostream& operator<<(ostream& out, person &printable);
     
 };
-ostream& operator<<(person &printable, ostream& out){
+ostream& operator<<(ostream& out, person &printable){
     
-    out<<printable.fullName<<" "<<printable.age<<" "<<printable.salary;
+    out<<"My Name is "<<printable.fullName<<" and my age is "<<printable.age<<" and my salary is "<<printable.salary<<endl;
     return out;
 }
 
@@ -42,13 +47,14 @@ ostream& operator<<(person &printable, ostream& out){
 
 
 int main(){
-    person dinaol;
-    person kirubel;
+    person dinaol("Dinaol", 20, 2001);
+    person kirubel("Kirubel", 25, 2222);
+    cout<<dinaol;
     dinaol = kirubel +dinaol;
     
-    
-    cout<<dinaol.getsalary() << endl;
-    cout<<"Dinaol"<<endl;
+    cout<<dinaol<<endl;
+    //cout<<dinaol.getsalary() << endl;
+    //cout<<"Dinaol"<<endl;
     
     return 0;
 }
